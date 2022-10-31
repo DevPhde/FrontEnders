2c5b8b2fed16a5812999634391d0b9f9efb8250a
Devair Martins <devair.martins@gmail.com>
2022-10-31 16:24:22 -0300

    api ok

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function limpa_formulário_cep() {
         //Limpa valores do formulário de cep.
-        document.getElementById('rua').value=("");
-        document.getElementById('bairro').value=("");
-        document.getElementById('cidade').value=("");
+        document.getElementById('street').value=("");
+        document.getElementById('district').value=("");
+        document.getElementById('city').value=("");
         document.getElementById('uf').value=("");
 }
 
 function meu_callback(conteudo) {
     if (!("erro" in conteudo)) {
         //Atualiza os campos com os valores.
-        document.getElementById('rua').value=(conteudo.logradouro);
-        document.getElementById('bairro').value=(conteudo.bairro);
-        document.getElementById('cidade').value=(conteudo.localidade);
+        document.getElementById('street').value=(conteudo.logradouro);
+        document.getElementById('district').value=(conteudo.bairro);
+        document.getElementById('city').value=(conteudo.localidade);
         document.getElementById('uf').value=(conteudo.uf);
     } //end if.
     else {
@@ -36,9 +36,9 @@ function pesquisaCep(valor) {
         if(validacep.test(cep)) {
 
             //Preenche os campos com "..." enquanto consulta webservice.
-            document.getElementById('rua').value="...";
-            document.getElementById('bairro').value="...";
-            document.getElementById('cidade').value="...";
+            document.getElementById('street').value="...";
+            document.getElementById('district').value="...";
+            document.getElementById('city').value="...";
             document.getElementById('uf').value="...";
 
             //Cria um elemento javascript.
