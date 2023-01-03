441c2cbabd2aeae6df8eb5a9fdc3baad8fa1a6a7
Diego Baumbach <dbsmendes@gmail.com>
2023-01-03 20:23:13 -0300

    Login UI - Finished Integration with ServerSide

---
+++ script/index.js
@@ -0,0 +1,39 @@
+let email = document.getElementById("floatingInput")
+let password = document.getElementById("floatingPassword")
+const form = document.querySelector("[data-form]")
+const userError = document.getElementById("userError")
+
+
+class User {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+    static createUser() {
+        const user = new User(email.value, password.value)
+        return user
+    }
+}
+
+async function connect() {
+    const conexao = await fetch('https://authentication-api-pvz6.onrender.com/v1/login', {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(User.createUser())
+    });
+    const conexaoConvertida = await conexao.json();
+    if (conexaoConvertida.result) {
+        console.log("liberado")
+    } else {
+        userError.innerHTML = conexaoConvertida.message
+    }
+
+    return conexaoConvertida;
+}
+
+form.addEventListener('submit', (e) => {
+    connect()
+    e.preventDefault()
+})
