634c8597d2bb375b01e95aa599a3909a09137a6f
Diego Baumbach <dbsmendes@gmail.com>
2023-01-15 04:37:47 -0300

    Dashboard - Improve code

--- script/dashboard.js
+++ script/dashboard.js
@@ -38,17 +38,17 @@ let ctx1 = document.getElementById('bar-chart').getContext('2d');
 let data1 = {
     labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul"],
     datasets: [{
-        label: "Valor das Ações",
+        label: "Transações",
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: [500, 590, 800, 810, 560, 550, 400,20]
     }]
 };
 
 let barChart = new Chart(ctx1, {
     type: 'bar',
-    data: data,
+    data: data1,
     options: {}
 });
 
