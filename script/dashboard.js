cd318128d7766719bdb53093a4c4b5f16b58f2e0
Diego Baumbach <dbsmendes@gmail.com>
2023-01-15 04:37:17 -0300

    Dashboard - Adjusting code

--- script/dashboard.js
+++ script/dashboard.js
@@ -3,7 +3,7 @@ import { Routes } from "./routing/routes.js"
 
 
 window.onload = async () => {
-    const render = await Routes.Dashboard()
+    // const render = await Routes.Dashboard()
     if(render) {
         const body = document.querySelector('body')
         body.hidden = false
@@ -38,17 +38,17 @@ let ctx1 = document.getElementById('bar-chart').getContext('2d');
 let data1 = {
     labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul"],
     datasets: [{
-        label: "Valor das Ações",
+        label: "Transações",
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: [500, 590, 800, 810, 560, 550, 400,20]
     }]
 };
 
 let barChart = new Chart(ctx1, {
     type: 'bar',
-    data: data,
+    data: data1,
     options: {}
 });
 
