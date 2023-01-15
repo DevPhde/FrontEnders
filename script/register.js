daf32d2b81b26e5fdc1bfbaa06007c1c466b1238
Diego Baumbach <dbsmendes@gmail.com>
2023-01-15 01:45:51 -0300

    Register - Improve js code

--- script/register.js
+++ script/register.js
@@ -3,5 +3,5 @@ import { Routes } from "./routing/routes.js"
 const form = document.querySelector("[data-form]")
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
-    const dados = await Routes.UserRegister()
+    Routes.UserRegister()
 })
